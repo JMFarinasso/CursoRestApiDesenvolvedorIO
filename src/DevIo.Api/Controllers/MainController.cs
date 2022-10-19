aa1ad7b34aaa7e694db38887e342fcfcc2b47993
Joao Farinasso <joaomfarinasso@gmail.com>
2022-10-19 10:14:18 -0300

    atualização do projeto até a aula de modelagem da controller de fornecedores

--- src/DevIo.Api/Controllers/MainController.cs
+++ src/DevIo.Api/Controllers/MainController.cs
@@ -11,24 +11,6 @@ namespace DevIo.Api.Controllers
         
         // validacao de modelstate
 
-        // validacao de operacao de negocios
-    };
-
-    [Route("api/controller")]
-    public class FornecedoresController : MainController
-    {
-        private readonly IFornecedorRepository _fornecedorRepository;
-
-        public FornecedoresController(IFornecedorRepository fornecedorRepository)
-        {
-            _fornecedorRepository = fornecedorRepository;   
-        }
-
-        public async Task<ActionResult<IEnumerable<FornecedorViewModel>>> ObterTodos()
-        {
-            var fornecedores = await _fornecedorRepository.ObterTodos();
-
-            return Ok(fornecedores);
-        }
-    }
+        // validacao da operacao de negocios
+    };    
 }
--- src/DevIo.Api/Program.cs
+++ src/DevIo.Api/Program.cs
@@ -1,3 +1,7 @@
+using DevIo.Api.Configuration;
+using DevIO.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +11,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<MeuDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.ResolveDependencies();
 
 var app = builder.Build();
 
