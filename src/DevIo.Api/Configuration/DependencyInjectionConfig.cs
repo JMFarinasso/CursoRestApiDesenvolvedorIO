d3e95b7be0de7b4c2a3e2120f4b72992ded8a507
Joao Farinasso <joaomfarinasso@gmail.com>
2022-10-19 10:38:32 -0300

    Atualização gitignore e DI config

--- src/DevIo.Api/Configuration/DependencyInjectionConfig.cs
+++ src/DevIo.Api/Configuration/DependencyInjectionConfig.cs
@@ -1,4 +1,6 @@
 ﻿using DevIO.Business.Intefaces;
+using DevIO.Business.Notificacoes;
+using DevIO.Business.Services;
 using DevIO.Data.Context;
 using DevIO.Data.Repository;
 
@@ -8,8 +10,26 @@ namespace DevIo.Api.Configuration
     {
         public static IServiceCollection ResolveDependencies(this IServiceCollection services)
         {
+            #region DbContext
             services.AddScoped<MeuDbContext>();
+            #endregion
+
+            #region Repository
             services.AddScoped<IFornecedorRepository, FornecedorRepository>();
+            services.AddScoped<IEnderecoRepository, EnderecoRepository>();
+            services.AddScoped<IProdutoService, ProdutoService>();
+            #endregion
+
+
+            #region Services
+            services.AddScoped<IFornecedorService, FornecedorService>();
+            services.AddScoped<IProdutoService, ProdutoService>();
+            #endregion
+
+            #region Notificador
+            services.AddScoped<INotificador, Notificador>();
+            #endregion
+
             return services;
         }
     }
